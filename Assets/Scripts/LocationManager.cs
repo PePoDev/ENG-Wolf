56373f735d42065ba135ba36e2a0abe25663d713
Thiwanon Chomcharoen <gmgame.1525@gmail.com>
2019-07-04 12:07:13 +0700

    Init project

---
+++ Assets/Scripts/LocationManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocationManager : MonoBehaviour
+{
+	public static LocationManager Instance { set; get; }
+	public float latitude, longitude;
+	public GameObject ui_ground;
+
+	private void Start()
+	{
+		Instance = this;
+		DontDestroyOnLoad(gameObject);
+		StartCoroutine(StartLocationService());
+	}
+
+	private IEnumerator StartLocationService()
+	{
+		if (!Input.location.isEnabledByUser)
+		{
+			Debug.Log("User has not enabled GPS");
+			yield break;
+		}
+
+		Input.location.Start();
+		int maxWait = 20;
+		while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+		{
+			yield return new WaitForSeconds(1);
+			maxWait--;
+		}
+
+		if (maxWait <= 0)
+		{
+			Debug.Log("Timed Out!");
+			yield break;
+		}
+
+		if (Input.location.status == LocationServiceStatus.Failed)
+		{
+			Debug.Log("Failed to Loaded!");
+			yield break;
+		}
+
+		latitude = Input.location.lastData.latitude;
+		longitude = Input.location.lastData.longitude;
+
+		if (latitude <= 13.9f && longitude <= 100.56)
+		{
+			ui_ground.SetActive(true);
+		}
+		else
+		{
+			ui_ground.SetActive(false);
+		}
+		yield break;
+	}
+}
---
+++ Assets/Scripts/Scene Manager/MenuSceneManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuSceneManager : MonoBehaviour
+{
+	private void Start()
+	{
+
+	}
+
+	private void Update()
+	{
+		
+	}
+
+	#region OnButtonClick_Event
+
+	public void GameStart()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+	#endregion
+}
